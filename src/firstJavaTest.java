f491d0fda27631f5a4ba206a8df4c3b97bfd30b4
smoss <butthead@beavis.com>
2017-10-08 01:26:18 -0600

    First commit - testing.

---
+++ src/firstJavaTest.java
@@ -0,0 +1,7 @@
+public class firstJavaTest {
+    public static void main(String[] args) {
+        System.out.println("Hello Steve!");
+        System.out.println("This is Java doing the work.");
+        System.out.println("After git initiated.");
+    }
+}
